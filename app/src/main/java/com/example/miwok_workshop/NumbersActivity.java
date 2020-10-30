264d05deba1b858972ec9057b041357e96b47e7f
Navyashree R Bhat <navyashreerbhat1204@gmail.com>
2020-10-30 15:50:47 +0530

    Multiscreen app

--- app/src/main/java/com/example/miwok_workshop/NumbersActivity.java
+++ app/src/main/java/com/example/miwok_workshop/NumbersActivity.java
@@ -17,16 +17,16 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         final List<MiwokModel> number=new ArrayList<>();
-        number.add(new MiwokModel("one","lutti", R.drawable.number_one));
-        number.add(new MiwokModel("two","otiiko", R.drawable.number_two));
-        number.add(new MiwokModel("three","tolookosu",R.drawable.number_three));
-        number.add(new MiwokModel("four","oyyisa",R.drawable.number_four));
-        number.add(new MiwokModel("five","massokka",R.drawable.number_five));
-        number.add(new MiwokModel("six","temmokka",R.drawable.number_six));
-        number.add(new MiwokModel("seven","kenekaku",R.drawable.number_seven));
-        number.add(new MiwokModel("eight","kawinta",R.drawable.number_eight));
-        number.add(new MiwokModel("nine","wo'e",R.drawable.number_nine));
-        number.add(new MiwokModel("ten","na'aacha",R.drawable.number_ten));
+        number.add(new MiwokModel("One","lutti", R.drawable.number_one));
+        number.add(new MiwokModel("Two","otiiko", R.drawable.number_two));
+        number.add(new MiwokModel("Three","tolookosu",R.drawable.number_three));
+        number.add(new MiwokModel("Four","oyyisa",R.drawable.number_four));
+        number.add(new MiwokModel("Five","massokka",R.drawable.number_five));
+        number.add(new MiwokModel("Six","temmokka",R.drawable.number_six));
+        number.add(new MiwokModel("Seven","kenekaku",R.drawable.number_seven));
+        number.add(new MiwokModel("Eight","kawinta",R.drawable.number_eight));
+        number.add(new MiwokModel("Nine","wo'e",R.drawable.number_nine));
+        number.add(new MiwokModel("Ten","na'aacha",R.drawable.number_ten));
 
         RecyclerView numbers_recyclerView=findViewById(R.id.recyclerView3);
         LinearLayoutManager layoutManager=new LinearLayoutManager(this);
